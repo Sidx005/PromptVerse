2d72f021e66378ae54933f3fddd4009b126706eb
Sidx005 <desaisiddharth1246@gmail.com>
2025-03-13 17:00:06 +0530

    First commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,20 @@
 
 import Nav from '@components/Nav'
 import Provider from '@components/Provider'
+import Head from '@node_modules/next/head'
 // import { SessionProvider } from '@node_modules/next-auth/react'
 import '@styles/globals.css'
 export const metadata={
     title:"PromptVerse",
-    description:'Discover & Sharre AI prompts'
+    description:'Discover & Sharre AI prompts',
+    icons: {
+      icon: "/favicon.png", // or "/favicon.png"
+    },
 }
 const layout = ({children}) => {
   return (
     <html lang='en'>
+   
     <body>
       <Provider>
         <div className="main">
